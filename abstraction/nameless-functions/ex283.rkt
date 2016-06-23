c7f6850b2fb2353938d35d1c13cc234e04c95108
Jiang Peng <jiangpeng.nju@gmail.com>
2016-06-23 18:27:30 +0800

    finished till ch20

---
+++ abstraction/nameless-functions/ex283.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex283) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Use compass-and-pencil drawing to check the tests.
+
+
+
+; OK
